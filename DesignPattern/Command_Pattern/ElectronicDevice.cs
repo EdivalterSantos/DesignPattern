8a83a90801082828433ef695da121cae7f76cad8
Edivalter Dos Santos <edivalter.dos-santos@optelgroup.com>
2018-04-10 14:51:31 -0400

    new command

---
+++ DesignPattern/Command_Pattern/ElectronicDevice.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Command_Pattern
+{
+    interface ElectronicDevice
+    {
+        void DeviceOn();
+
+        void DeviceOff();
+
+        void VolumeUp();
+
+        void VolumeDown();
+    }
+}
