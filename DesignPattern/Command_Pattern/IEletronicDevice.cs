60dbaf94686d8d11c9b8bc2259184dc6a903a27d
Edivalter Dos Santos <edivalter.dos-santos@optelgroup.com>
2018-04-10 11:43:43 -0400

    command pattern start

---
+++ DesignPattern/Command_Pattern/IEletronicDevice.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Command_Pattern
+{
+    interface IEletronicDevice
+    {
+        void On();
+
+        void Off();
+
+        void VolumeUp();
+
+        void VolumeDown();
+    }
+}
---
+++ DesignPattern/Command_Pattern/Television.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Command_Pattern
+{
+    public class Television : IEletronicDevice
+    {
+        public void On()
+        {
+
+        }
+
+        public void Off()
+        {
+
+        }
+
+        public void VolumeUp()
+        {
+
+        }
+
+        public void VolumeDown()
+        {
+
+        }
+    }
+}
