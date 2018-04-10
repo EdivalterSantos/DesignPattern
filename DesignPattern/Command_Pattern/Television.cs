a6804c5d0eca84d9650c383b67e67d1c85706388
Edivalter Dos Santos <edivalter.dos-santos@optelgroup.com>
2018-04-10 11:54:00 -0400

    test merge

---
+++ DesignPattern/Command_Pattern/Television.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Command_Pattern
+{
+    public class Television : IEletronicDevice
+    {
+        public void On()
+        {
+            Console.WriteLine(" Tv on ");
+        }
+
+        public void Off()
+        {
+            Console.WriteLine(" Tv off ");
+        }
+
+        public void VolumeUp()
+        {
+            Console.WriteLine(" Volume up ");
+        }
+
+        public void VolumeDown()
+        {
+            Console.WriteLine(" Volume down ");
+        }
+    }
+}
